7c0665c1a00a141f3883ae0cf8a36d62951bb3bd
zhongwangwei <lifeecho@live.com>
2014-11-05 15:28:21 +0900

    Create main.m
    The main program for flux estimation

---
+++ main.m
@@ -0,0 +1,31 @@
+%# This is a program to calculate fluxes
+%# Coded by Zhongwang Wei on 2014/11/02 @Tokyo Univ.
+%# Modified by Zhongwang Wei on 2014/11/04 @Tokyo Univ.
+
+
+
+
+K=input('Please select the result:\ 1.original result; 2.corrected (rotation); 3.corrected (rotation+WPL):  ');
+[qq,tt,wwb,q,U,WD,u_star,wt,wq,wc,Ta,P]=fluxcalculation(data,K); %flux calculation
+ %obukhov length
+%Z =(10.8-Zh)./L;   %obukhov scale length
+T = Ta + 273.15 ; % ! [K];
+e = q .* T./ 0.2167;  % ! [hPa]
+
+rho98=1293 * 273.15./T .* (1013.25 - 0.378*e) / 1013.25 ;     %! [g/m3]
+Fb=((9.8.*( wt+ (0.61*(Ta).*wq)./(rho98)))./(Ta));
+ZL=(3)./((-u_star.^3)./(0.4.*Fb)); %warning！ the measurement height should be modified！！！
+
+ [RN_uw,RN_vw,dfw,dft,dfq]=ECcontrol(data,ZL,wwb,wt,wq,u_star,tt,qq);
+
+
+
+r = 1.293 * 273.15./T .* (1013.25 - 0.378*e) / 1013.25;   %! [kg/m3]
+Cp_r = 1005. * (r + 0.84*q) ;  %! [J/m3/K]
+Lv = 3151.05 - 2.37839*T ;  %! [J/kg]
+
+
+ H = Cp_r.* wt  ;      %W/m2 sensible heat
+ LE = Lv .* wq  ;      %W/m2 latent heat
+ C = 1000 .* wc;       %μmol/m2/s co2 flux
+
