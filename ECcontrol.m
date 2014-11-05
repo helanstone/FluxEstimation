d14fb5e85f55bdcfa4fc6060ff257bda04ddd3ac
zhongwangwei <lifeecho@live.com>
2014-11-05 18:11:12 +0900

    Update ECcontrol.m

--- ECcontrol.m
+++ ECcontrol.m
@@ -1,5 +1,5 @@
 function [RN_uw,RN_vw,RN_wt,RN_wq,RN_wc,dfw,dft,dfq]=ECcontrol(data,ZL,wwb,wt,wq,Ustar,Ts_v,H2O_v)
-[Ux,Uy,Uz,Ts,Q0,CO2]=cospdataread(data);
+[Ux,Uy,Uz,Ts,Q0,CO2]=datatoread(data);
 [RN_uw,RN_vw,RN_wt,RN_wq,RN_wc]=ttest(data); 
 [dfw,dft,dfq]=ITC(ZL,wwb,wt,wq,Ustar,Ts_v,H2O_v);
 
