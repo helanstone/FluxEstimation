7066011c6de8af13fdb8181af203186e3303823f
Zhongwang Wei <lifeecho@live.com>
2014-11-10 17:02:10 +0900

    add code comment

--- main.m
+++ main.m
@@ -4,11 +4,10 @@
 
 
 
-selectH=input('Please select the observation height:\ (1) 2m; (2) 15m; (3) 30m:  ');
-K=input('Please select the result:\ 1.original result; 2.corrected (rotation); 3.corrected (rotation+WPL):  ');
+selectH=input('Please select the observation height:\ (1) 2m; (2) 15m; (3) 30m:  '); % select the observation high
+K=input('Please select the result:\ 1.original result; 2.corrected (rotation); 3.corrected (rotation+WPL):  '); % choose the 
 [qq,tt,wwb,q,U,WD,u_star,wt,wq,wc,Ta,P]=fluxcalculation(data,K); %flux calculation
- %obukhov length
-%Z =(10.8-Zh)./L;   %obukhov scale length
+
 T = Ta + 273.15 ; % ! [K];
 e = q .* T./ 0.2167;  % ! [hPa]
 
@@ -22,17 +21,18 @@ else
 printf('error');
 end
 
-rho98=1293 * 273.15./T .* (1013.25 - 0.378*e) / 1013.25 ;     %! [g/m3]
-Fb=((9.8.*( wt+ (0.61*(Ta).*wq)./(rho98)))./(Ta));
-ZL=(selectH)./((-u_star.^3)./(0.4.*Fb)); %warning！ the measurement height should be modified！！！
-
- [RN_uw,RN_vw,RN_wt,RN_wq,RN_wc,dfw,dft,dfq]=ECcontrol(data,ZL,wwb,wt,wq,u_star,tt,qq);
+rho98=1293 * 273.15./T .* (1013.25 - 0.378*e) / 1013.25 ;     %! [g/m3] vapor density
+Fb=((9.8.*( wt+ (0.61*(Ta).*wq)./(rho98)))./(Ta)); %obukhov length
+ZL=(selectH)./((-u_star.^3)./(0.4.*Fb)); %obukhov length
 
 
+ [RN_uw,RN_vw,RN_wt,RN_wq,RN_wc,dfw,dft,dfq]=ECcontrol(data,ZL,wwb,wt,wq,u_star,tt,qq); % data control
 
-r = 1.293 * 273.15./T .* (1013.25 - 0.378*e) / 1013.25;   %! [kg/m3]
-Cp_r = 1005. * (r + 0.84*q) ;  %! [J/m3/K]
-Lv = 3151.05 - 2.37839*T ;  %! [J/kg]
+
+
+r = 1.293 * 273.15./T .* (1013.25 - 0.378*e) / 1013.25;   %! [kg/m3] vapor density
+Cp_r = 1005. * (r + 0.84*q) ;  %! [J/m3/K] specific heat at constant pressure
+Lv = 3151.05 - 2.37839*T ;  %! [J/kg] latent heat of vaporization 
 
 
  H = Cp_r.* wt  ;      %W/m2 sensible heat
