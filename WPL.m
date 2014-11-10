44d0df26972b44f6920da8c21bdb7cf53b400b2a
Zhongwang Wei <lifeecho@live.com>
2014-11-10 17:05:32 +0900

    Update WPL.m

--- WPL.m
+++ WPL.m
@@ -1,7 +1,7 @@
 function [wc,wq]=WPL(Tair_1_AVG,H2O_a,Press_AVG,CO2_a,wq,wt,wc)
 
-Twpl = Tair_1_AVG + 273.15;                             
-ewpl=((H2O_a.*Twpl)./216.7)*0.018;                      
+Twpl = Tair_1_AVG + 273.15;                      % temperature at K       
+ewpl=((H2O_a.*Twpl)./216.7)*0.018;                %      
 rdwpl=((1293*273.15)./(Twpl)).*((Press_AVG-0.378*ewpl)./1013.25);   
 %rdwpl=((1293*273.15)./(Twpl)).*((Press_AVG)./1013.25);  
 kk=(1.608*H2O_a* 0.018)./rdwpl;                                     
